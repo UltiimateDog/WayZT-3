4a9de5bf7340fdfd6448758a5acc027995e24087
UltiimateDog <alan.perezhz@icloud.com>
2025-04-01 14:52:42 +0100

    Initial Commit

---
+++ WayZT 3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WayZT 3
+//
+//  Created by Ultiimate Dog on 01/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WayZT 3/WayZT_3App.swift
@@ -0,0 +1,17 @@
+//
+//  WayZT_3App.swift
+//  WayZT 3
+//
+//  Created by Ultiimate Dog on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct WayZT_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
